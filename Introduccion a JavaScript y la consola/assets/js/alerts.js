8823eef5f08226954361973464951ade1a0dab03
DubanPrograDVR <156736859+DubanPrograDVR@users.noreply.github.com>
2025-09-11 11:52:29 -0300

    Termino Primera Parte

---
+++ Introduccion a JavaScript y la consola/assets/js/alerts.js
@@ -0,0 +1,9 @@
+// alert("Hola Mundo");
+
+// let nombre = prompt("¿Cual es tu nombre?", "Sin Nombre");
+// console.log("****", nombre + "****");
+
+// const seleccion = confirm("Estas suguro de borrar esto?");
+// console.log(seleccion);
+
+console.log(global);
---
+++ Introduccion a JavaScript y la consola/assets/js/app.js
@@ -0,0 +1,24 @@
+console.log(minuevonombre + "Duban");
+
+// alert("Hola Desde App.js");
+
+console.log("Hola Mundo");
+
+// let a = 10,
+//   b = 20,
+//   c = "Hola",
+//   d = "Spiderman",
+//   x = a + b;
+
+const saludo = c + " " + d;
+
+// console.log("%c Mis Variables", "color:blue; font-weight:bold");
+// console.log({ a });
+// console.log({ b });
+// console.log({ c });
+
+c = "Hola Amigos";
+
+// console.table({ a, b, c, d, x });
+
+let minuevonombre = "Duban Vasquez ";
