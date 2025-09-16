c0de789861be661514aec27d02ab73c1b0276195
DubanPrograDVR <156736859+DubanPrograDVR@users.noreply.github.com>
2025-09-16 12:17:28 -0300

    Protips del profe

---
+++ Fundamentos de JavaScript, primitivos, arreglos, objetos y funciones básicas/assets/js/protip-retorno.js
@@ -0,0 +1,48 @@
+// function crearPersona(nombre, apellido) {
+//   return { nombre, apellido };
+// }
+
+const crearPersona = (nombre, apellido) => ({ nombre, apellido });
+
+const persona = crearPersona("Duban", "Vasquez");
+console.log(persona);
+
+function imprimeArgumentos() {
+  console.log(arguments);
+}
+
+const imprimeArgumentos2 = (...args) => {
+  //   console.log(args);
+  return args;
+};
+
+const [casado, vivo, nombre, saludo] = imprimeArgumentos2(
+  10,
+  true,
+  false,
+  "False",
+  "hola"
+);
+
+console.log(casado, vivo, nombre, saludo);
+
+const { apellido: nuevoApellido } = crearPersona("Duban", "Romero");
+console.log({ nuevoApellido });
+
+const tony = {
+  nombre: "Tony Stark",
+  codeName: "Iron man",
+  vivo: false,
+  edad: 40,
+  traje: ["Marck I", "Marck V", "Hulkbuster"],
+};
+
+const imprimePropiedades = ({ nombre, codeName, vivo, edad, trajes }) => {
+  console.log({ nombre });
+  console.log({ codeName });
+  console.log({ vivo });
+  console.log({ edad });
+  console.log({ trajes });
+};
+
+console.log(tony);
